d57cf0b3dc14fd3eadf475546f853c72ac53da72
Gonzalo Diaz <info@ventra.com.co>
2019-03-06 16:40:01 +0000

    done

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,12 +1,18 @@
 // app/javascript/components/navbar.js
 const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-hoxton');
+  const gray = document.querySelector('.gray-logo');
+  const white = document.querySelector('.white-logo');
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
+        gray.style.display = "block"
+        white.style.display = "none"
         navbar.classList.add('navbar-hoxton-white');
       } else {
         navbar.classList.remove('navbar-hoxton-white');
+        gray.style.display = "none"
+        white.style.display = "block"
       }
     });
   }
